b2ed5e04a3dc895706f313e1bd8016f9dfe948aa
patricewhite <patricewhite2012@gmail.com>
2017-06-12 16:16:38 -0400

    intial test passed

--- server.js
+++ server.js
@@ -2,4 +2,11 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html');
+});
+
 app.listen(process.env.PORT || 8080);
+
+module.exports = {app}
---
+++ test/test-server.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+const{app} = require('../server')
+chai.use(chaiHttp);
+
+describe('Get endpoint', function(){
+  it('very you hit root url', function(){
+    return chai.request(app)
+    .get('/')
+    .then(res => {
+      res.should.have.status(200);
+    });
+  });
+});
