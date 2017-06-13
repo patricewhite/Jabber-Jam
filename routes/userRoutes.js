844972402e11805a7f0ac0f3c84245955cab154a
patricewhite <patricewhite2012@gmail.com>
2017-06-13 12:06:16 -0400

    can create user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,23 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
 const jsonParser = bodyParser.json();
+const mongoose = require('mongoose');
 
-const{Users} = require('../models/models');
+const{User} = require('../models/chatroom');
 
 
-router.post('/users',jsonParser, (req, res) => {
-  // const requiredFields = ['username'];
-  // for (let i = 0; i < requiredFields.length; i++) {
-  //   const field = requiredFields[i];
-  //   if (!(field in req.body)) {
-  //     const message = `Missing ${field} in request body.`;
-  //     console.error(message);
-  //     return res.status(400).send(message);
-  //   }
-  // }
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['username', 'password', 'email'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    console.log(field);
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body.`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
 
-  const user = Users.create(req.body.username);
-  res.status(201).json(user);
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  })
+  .then(
+    user => {
+      console.log(user);
+      res.status(201).json(user.apiRepr());
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
 });
 
 
--- server.js
+++ server.js
@@ -3,17 +3,61 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const userRouter = require('./routes/userRoutes');
+const chatRouter = require('./routes/userRoutes');
 const {DATABASE_URL, PORT} = require('./config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
 
 app.use(express.static('public'));
 app.use('/users', userRouter);
+app.use('/chatrooms', chatRouter);
 app.use(morgan('common'));
 
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html');
 });
 
+let server;
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
 
-app.listen(process.env.PORT || 8080);
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
 
-module.exports = {app};
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
+
+// app.listen(process.env.PORT || 8080);
+//
+// module.exports = {app};
