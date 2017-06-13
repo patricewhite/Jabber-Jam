6bf627471a2eaf8765faeff952f4ea58a518031a
patricewhite <patricewhite2012@gmail.com>
2017-06-12 20:21:39 -0400

    user routes

--- server.js
+++ server.js
@@ -2,10 +2,14 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+const userRouter = require('./routes/userRoutes');
 
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html');
 });
+
+app.use('/users', userRouter);
+
 
 app.listen(process.env.PORT || 8080);
 
