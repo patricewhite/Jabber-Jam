124eadd06631635b6f2c6c17098ece7a4d6cd2b7
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-06-14 11:23:24 -0700

    got the client to update based on database.

--- public/mainscreen.js
+++ public/mainscreen.js
@@ -3,12 +3,13 @@
 //////////////////////////////////////////////////////////////
 ///////////////          Helper Functions       /////////////
 ////////////////////////////////////////////////////////////
+/* add data to the database chatroom */
 function addToData(element){
   const obj = {
     title:element.find('#title').val(),
     category:element.find('#category').val()
   };
-  fetch('http://jabber-jam.herokuapp.com/chatrooms',{
+  return fetch('https://jabber-jam.herokuapp.com/chatrooms',{
     method: 'POST',
     mode:'cors',
     headers: new Headers({
@@ -21,17 +22,15 @@ function addToData(element){
       return Promise.reject(res.statusText);
     }
     return res.json();
-  })
-  .then(resQ =>{
-    alert(`You have created the chatroom with the title ${resQ.title} and category ${resQ.category}`);
-  }); 
+  });
 }
+
 //////////////////////////////////////////////////////////////
 ///////////////        State Modification       /////////////
 ////////////////////////////////////////////////////////////
-function addDataChatroom(state){
+function getDataChatroom(state){
 
-  fetch('https://jabber-jam.herokuapp.com/chatrooms',{
+  return fetch('https://jabber-jam.herokuapp.com/chatrooms',{
     method: 'GET',
     mode:'cors',
     headers: new Headers({
@@ -43,28 +42,48 @@ function addDataChatroom(state){
       return Promise.reject(res.statusText);
     }
     return res.json();
-  })
-  .then(resQ=>{
-    state.chatroomList()
   });
 }
+
 //////////////////////////////////////////////////////////////
 ///////////////          Render                 /////////////
 ////////////////////////////////////////////////////////////
+function render(){
 
+}
+
+function renderChatroomList(state,element){
+  getDataChatroom(state)
+  .then(resQ=>{
+    state.chatroomList = resQ;
+    let htmlStr = `<p>Chatroom List</p>
+          <ul>`;
+    const mappedChat = state.chatroomList.map(el=>{
+      return `<li><a href="chatroom.html" class="chatroom_link">${el.title}</a></li>`;
+    }).join('\n\t');
+
+    htmlStr += mappedChat + `</ul>`;
+    element.html(htmlStr);
+  });
+}
 //////////////////////////////////////////////////////////////
 ///////////////          Event Listeners        /////////////
 ////////////////////////////////////////////////////////////
-function createChatroom(){
+function createChatroom(state){
   $('.chatroom_form').on('submit',function(event){
     event.preventDefault();
-    addToData($('.chatroom_form'));
-    
+    addToData($('.chatroom_form'))
+    .then((resQ)=>{
+      alert(`You have created the chatroom with the title ${resQ.title} and category ${resQ.category}`);
+      renderChatroomList(state,$('.list_chatroom'));
+    });
   });
 }
+
 //////////////////////////////////////////////////////////////
 ///////////////          Callback Function      /////////////
 ////////////////////////////////////////////////////////////
 $(function(){
-  createChatroom();
+  renderChatroomList(appState, $('.list_chatroom'));
+  createChatroom(appState);
 });
