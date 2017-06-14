ac16cb310567660621d4a5c311ef3ed790efae5a
patricewhite <patricewhite2012@gmail.com>
2017-06-14 17:47:17 -0400

    recieve messages

--- public/chatroom.js
+++ public/chatroom.js
@@ -2,12 +2,12 @@
 
 //state mod function
 function getMessages(state){
-  return fetch('https://jabber-jam.herokuapp.com/chatrooms'), {
+  return fetch('https://jabber-jam.herokuapp.com/chatrooms', {
     method: 'GET',
     mode: 'cors',
     headers: new Headers({
       'Content-Type': "application/json"
-    })
+    }),
   })
   then(res => {
     if(!res.ok) {
@@ -17,10 +17,45 @@ function getMessages(state){
   });
 }
 
+// function createMessages(state){
+//   return fetch('https://jabber-jam.herokuapp.com/chatrooms/59406dcccb46821720c90b29', {
+//     method: 'POST',
+//     mode: 'cors',
+//     headers: new Headers({
+//       'Content-Type': "application/json"
+//     }),
+//     body: JSON.stringify(object)
+//   })
+//   then(res => {
+//     if(!res.ok) {
+//       return Promise.reject(res.statusText);
+//     }
+//     return res.json();
+//   });
+// }
 //render function
-function renderMessages(state,element){
+function renderRecievedMessages(state,element){
   getMessages(state)
   .then(resM => {
-    state.sentMessages
+    const message = state.sentMessages.map(el => {
+      return `<li>${el.messages}</li>`;
+    }).join('\n');
+    element.html(message);
+  });
+};
+
+// function renderSentMessages(state,element){
+//
+// }
+//eventlistener
+function watchSubmit(state){
+  $('#button').on('submit', function(event){
+    event.preventDefault();
+    renderRecievedMessages(appState, $('.conversation'));
   })
 }
+
+$(function() {
+  renderRecievedMessages(appState, $('.conversation'));
+  watchSubmit(appState);
+})
