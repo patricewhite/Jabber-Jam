c3706f34b7451900b7d93fb2445263c47cbd8ca8
patricewhite <patricewhite2012@gmail.com>
2017-09-06 20:51:37 -0400

    taking oauth off backend

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -19,82 +19,82 @@ const {ChatRoom, User} = require('../models/chatroom');
 router.use(jsonParser);
 
 /*Password Import stuff */
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const BearerStrategy = require('passport-http-bearer').Strategy;
-
-let secret = {
-  CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  DATABASE_URL: process.env.DATABASE_URL
-};
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const BearerStrategy = require('passport-http-bearer').Strategy;
+//
+// let secret = {
+//   CLIENT_ID: process.env.CLIENT_ID,
+//   CLIENT_SECRET: process.env.CLIENT_SECRET,
+//   DATABASE_URL: process.env.DATABASE_URL
+// };
 
 if (process.env.NODE_ENV !== 'production') {
   secret = require('../secret');
 }
 
-passport.use(new GoogleStrategy({
-  clientID: secret.CLIENT_ID,
-  clientSecret: secret.CLIENT_SECRET,
-  callbackURL: '/api/auth/google/callback'
-}, (accessToken, refreshToken, profile, cb) => {
-  User.findOneAndUpdate({
-    googleId: profile.id
-  }, {
-    $set: {
-      googleId: profile.id,
-      accessToken: accessToken
-    }
-  }, {
-    new: true,
-    upsert: true
-  }, (err, user) => {
-    console.log('!!!!!!!!!', user);
-    return cb(err, user);
-  });
-}));
-
-passport.use(new BearerStrategy((token, done) => {
-  User.findOne({
-    accessToken: token
-  }, (err, user) => {
-    if (err) {
-      return done(err);
-    }
-    if (!user) {
-      return done(null, false);
-    }
-    return done(null, user, {scope: 'all'});
-  });
-}));
+// passport.use(new GoogleStrategy({
+//   clientID: secret.CLIENT_ID,
+//   clientSecret: secret.CLIENT_SECRET,
+//   callbackURL: '/api/auth/google/callback'
+// }, (accessToken, refreshToken, profile, cb) => {
+//   User.findOneAndUpdate({
+//     googleId: profile.id
+//   }, {
+//     $set: {
+//       googleId: profile.id,
+//       accessToken: accessToken
+//     }
+//   }, {
+//     new: true,
+//     upsert: true
+//   }, (err, user) => {
+//     console.log('!!!!!!!!!', user);
+//     return cb(err, user);
+//   });
+// }));
+//
+// passport.use(new BearerStrategy((token, done) => {
+//   User.findOne({
+//     accessToken: token
+//   }, (err, user) => {
+//     if (err) {
+//       return done(err);
+//     }
+//     if (!user) {
+//       return done(null, false);
+//     }
+//     return done(null, user, {scope: 'all'});
+//   });
+// }));
 
 /////////////////////////////////////////////////////////////////////////////////////
 ///////////////                Oauth Endpoints             /////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 
-router.get('/api/auth/google', passport.authenticate('google', {scope: ['profile']}));
-
-router.get('/api/auth/google/callback', passport.authenticate('google', {
-  failureRedirect: '/',
-  session: false
-}), (req, res) => {
-  res.cookie('accessToken', req.user.accessToken, {expires: 0});
-  res.redirect('/');
-});
-
-router.get('/api/auth/logout', (req, res) => {
-  req.logout();
-  res.clearCookie('accessToken');
-  res.redirect('/');
-});
-
-router.get('/api/me', passport.authenticate('bearer', {session: false}), (req, res) => res.json({googleId: req.user.googleId}));
+// router.get('/api/auth/google', passport.authenticate('google', {scope: ['profile']}));
+//
+// router.get('/api/auth/google/callback', passport.authenticate('google', {
+//   failureRedirect: '/',
+//   session: false
+// }), (req, res) => {
+//   res.cookie('accessToken', req.user.accessToken, {expires: 0});
+//   res.redirect('/');
+// });
+//
+// router.get('/api/auth/logout', (req, res) => {
+//   req.logout();
+//   res.clearCookie('accessToken');
+//   res.redirect('/');
+// });
+//
+// router.get('/api/me', passport.authenticate('bearer', {session: false}), (req, res) => res.json({googleId: req.user.googleId}));
 
 /////////////////////////////////////////////////////////////////////////////////////
 ///////////////                  Post Chatroom             /////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 /*Creating and adding it into database */
-router.post('/', passport.authenticate('bearer', {session: false}), (req, res) => {
+router.post('/', (req, res) => {
   const requiredFields = ['title', 'category'];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -116,7 +116,7 @@ router.post('/', passport.authenticate('bearer', {session: false}), (req, res) =
 ///////////////                  Get Chatroom             /////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 /*Getting all the chatrooms */
-router.get('/', passport.authenticate('bearer', {session: false}), (req, res) => {
+router.get('/', (req, res) => {
   ChatRoom.find().exec().then(chats => {
     res.json(chats.map(chats => chats.apiRepr()));
   }).catch(err => {
@@ -126,7 +126,7 @@ router.get('/', passport.authenticate('bearer', {session: false}), (req, res) =>
 });
 
 /*Getting all distinct categories */
-router.get('/distinct', passport.authenticate('bearer', {session: false}), (req, res) => {
+router.get('/distinct', (req, res) => {
   ChatRoom.distinct("category").exec().then(chats => {
     console.log("chats", chats)
     res.json(chats);
@@ -137,7 +137,7 @@ router.get('/distinct', passport.authenticate('bearer', {session: false}), (req,
 });
 
 /*Getting a specific chatroom */
-router.get('/:id', passport.authenticate('bearer', {session: false}), (req, res) => {
+router.get('/:id', (req, res) => {
   ChatRoom.findById(req.params.id).exec().then(chats => {
     res.json(chats.apiRepr());
   }).catch(err => {
@@ -150,7 +150,7 @@ router.get('/:id', passport.authenticate('bearer', {session: false}), (req, res)
 ///////////////                  Put for Chatroom          /////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 /*Updating a document in the database */
-router.put('/:id', passport.authenticate('bearer', {session: false}), (req, res) => {
+router.put('/:id', (req, res) => {
   if (!(req.params.id === req.body.id)) {
     const message = (`Request path id (${req.params.id}) and request body id
       (${req.body.id}) must match`);
@@ -173,7 +173,7 @@ router.put('/:id', passport.authenticate('bearer', {session: false}), (req, res)
 ///////////////               Delete for Chatroom          /////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 /*Deleting a document in the database*/
-router.delete('/:id', passport.authenticate('bearer', {session: false}), (req, res) => {
+router.delete('/:id', (req, res) => {
   ChatRoom.findByIdAndRemove(req.params.id).exec().then(chat => res.status(204).end()).catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
